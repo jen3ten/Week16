1440a5ae0279e7dfccfa184bd4347a97389ee917
Jennifer Evans <jenevans3ten@gmail.com>
2016-04-27 18:08:28 -0400

    KLGradebook initial commit

---
+++ Week16_KLGradebookExample_04272016/Week16_KLGradebookExample_04272016/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week16_KLGradebookExample_04272016.MvcApplication" Language="C#" %>
---
+++ Week16_KLGradebookExample_04272016/Week16_KLGradebookExample_04272016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week16_KLGradebookExample_04272016.Startup))]
+namespace Week16_KLGradebookExample_04272016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
