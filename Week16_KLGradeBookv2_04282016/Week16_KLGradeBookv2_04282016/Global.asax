c8f99f80f4eba18093e4540260ebd6b2f7790796
Jennifer Evans <jenevans3ten@gmail.com>
2016-04-28 18:25:54 -0400

    New Gradebook DB v2 initial commit

---
+++ Week16_KLGradeBookv2_04282016/Week16_KLGradeBookv2_04282016/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Week16_KLGradeBookv2_04282016.MvcApplication" Language="C#" %>
---
+++ Week16_KLGradeBookv2_04282016/Week16_KLGradeBookv2_04282016/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Week16_KLGradeBookv2_04282016.Startup))]
+namespace Week16_KLGradeBookv2_04282016
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
